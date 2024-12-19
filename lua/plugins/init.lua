2ca35826c610fcb5c02f70574399c4d6e7e10b98
Luca Maggio <lucamaggio1992@gmail.com>
2024-12-20 00:17:57 +0100

    feat(plugins): setup f-person/gitblame plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,13 @@
 return {
-	"folke/neodev.nvim",
-	"folke/which-key.nvim",
-	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	"f-person/git-blame.nvim",
+	event = "VeryLazy",
+	opts = {
+		enabled = true, -- if you want to enable the plugin
+		message_when_not_committed = "Oh please, commit this!",
+		message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+		date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+		virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+		highlight_group = "Question",
+		delay = 500, -- 1 second
+	},
 }
