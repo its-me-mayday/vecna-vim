6655e52bf8765ada1425160e23339316d2d05cb8
Luca Maggio <lucamaggio1992@gmail.com>
2024-12-20 02:16:15 +0100

    fix(colorscheme): setup a init.lua to install each plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,8 @@ vim.opt.tabstop = 4 -- number of visual spaces per TAB
 vim.opt.softtabstop = 4 -- number of spacesin tab when editing
 vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
 vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+
+-- colorscheme
+vim.o.termguicolors = true
+vim.o.background = "dark"
+vim.cmd.colorscheme("catppuccin-macchiato")
