bfa9b3ce6d2ba7251c5cf4f631cc7164b615a36a
Luca Maggio <lucamaggio1992@gmail.com>
2025-01-17 18:58:18 +0100

    fix(lua/plugins/telescope): fix neovim doesn't find telescope module when load configuration file telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,20 @@
-local builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fa', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope git files' })
-vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope grep selected string' })
-
-vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'Telescope git commits' })
-vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'Telescope git branches' })
-vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Telescope git status' })
-
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
+
+  config = function()
+    local builtin = require('telescope.builtin')
+    
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fa', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+    vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope git files' })
+    vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope grep selected string' })
+    
+    vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'Telescope git commits' })
+    vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'Telescope git branches' })
+    vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Telescope git status' })
+end
 }
